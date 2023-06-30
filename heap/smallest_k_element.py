1cb5055be830ba7a3292ae8973f934f3a561cc16
Dennis <zdq198654@gmail.com>
2023-06-29 21:54:33 -0400

    not finish

---
+++ heap/smallest_k_element.py
@@ -0,0 +1,24 @@
+# Find smallest k elements from an unsorted array of size n
+
+# solution 1:
+# sort it and return the first K element!
+# O(nlogn)
+
+
+
+# 1. what is the relationship between k and n?
+
+
+# solution 2:
+# 1.How to build a min-heap? heapify it O(n)
+# 2.keep poping out K elements. O(klog(n))
+
+
+# solution 3:
+# 1.use a max-heap?
+# 2. max-heap first K elements 
+# 3. compare the others elements to the first K elements, if it is smaller than heap, than update heap
+
+# solution 4:
+# 
+
